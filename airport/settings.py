9b84a6424adba0f80ad2912b0e9e1b7404f07bba
godofcode007 <tariq_muzamil@live.com>
2024-05-12 21:40:45 +0300

    chore: Update ALLOWED_HOSTS in settings.py

--- airport/settings.py
+++ airport/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "=-%8fxd$iruq!-p97mj_f)oa^(=!e5p!(_&r5$((mj^!o(hq+%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".vercel.app",
+    "127.0.0.1",
 ]
 
 AUTH_USER_MODEL = "flights.User"
